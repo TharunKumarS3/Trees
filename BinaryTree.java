94c3cb6f3087b28781909633985d4d34b64790c7
TharunKumarS3 <tharun28102000@gmail.com>
2023-09-23 17:04:07 +0530

    binary and bst

--- BinaryTree.java
+++ BinaryTree.java
@@ -127,6 +127,11 @@ class BinaryTree {
     tree.populate(scanner);
     tree.prettyDisplay();
 
+    // BST tree = new BST();
+    // int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+    // tree.populate(nums);
+    // tree.display();
+
   }
 
 }
