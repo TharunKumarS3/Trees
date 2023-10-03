d3835838c0b7c00ff76b99288a297672bdeef2d1
TharunKumarS3 <tharun28102000@gmail.com>
2023-10-03 14:10:57 +0530

    Leetcode

---
+++ SumRootToLeaf.java
@@ -0,0 +1,23 @@
+public class SumRootToLeaf {
+
+    //LEETCODE 129
+
+    int sumNumbers(TreeNode root)
+    {
+        return helper(root,0);
+    }
+    int helper(TreeNode node,int sum)
+    {
+        if(node==null)
+        return 0;
+
+        sum=sum*10+node.val;
+        if(node.left==null && node.right==null)
+        {
+            return sum;
+        }
+        return helper(node.left,sum)+helper(node.right, sum);
+
+    }
+    
+}
