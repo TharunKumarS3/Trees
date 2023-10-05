b93d43c502919791a0b59a364638ef81c60392cb
TharunKumarS3 <tharun28102000@gmail.com>
2023-10-05 15:31:32 +0530

    LeetCode

---
+++ Preorder.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Preorder {
+     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        helper(root,ans);
+        return ans;
+        
+    }
+    void helper(TreeNode node,List<Integer> ans)
+    {
+        if(node==null)
+        return ;
+
+        ans.add(node.val);
+        helper(node.left,ans);
+        helper(node.right,ans);
+
+        
+    
+    
+}
